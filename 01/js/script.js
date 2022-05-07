755e68c8688dca1d3d8e1784a438e1bfec4207e8
Dima <d1ctators74@yandex.ru>
2022-05-07 15:02:25 +0300

    first commit

---
+++ 01/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+// // ===============================
+// const calc = [];
+// calc[0] = +prompt('Введите число "a"');
+// calc[1] = +prompt('Введите число "b"');
+// calc[2] = +prompt('Введите число "c"');
+
+// const sum = calc[0] + calc[1] + calc[2];
+// const umj = calc[0] * calc[1] * calc[2];
+// const min = calc[0] - calc[1] - calc[2];
+// const del = calc[0] / calc[1] / calc[2];
+
+// console.log('Результат суммы: ' + sum);
+// console.log('Результат умножения: ' + umj);
+// console.log('Результат вычитания: ' + min);W
+// console.log('Результат деления: ' + del);
+
+// // ===============================
+// let incr = 10,
+// 	decr = 10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// // ===============================
+// console.log(5 % 2);
+
+// // ===============================
+// console.log(2 * 4 === '8');
+
+// // ===============================
+// const isChecked = true,
+// 	isClose = true;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+
+// // ===============================
+
