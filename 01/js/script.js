a0ae94c28e0e19e649d3396e400f87d15f3d6d16
Dima <d1ctators74@yandex.ru>
2022-05-09 19:37:45 +0300

    pract-2

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,18 +1,40 @@
 "use strict";
 
-// let num = 50;
+const numberOfFilms = +prompt('Сволько фильмов уже посмотрели?', '');
 
-// while (num <= 55) {
-// 	console.log(num);
-// 	num++;
-// }
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-// do {
-// 	console.log(num);
-// 	num++;
-// }
-// while (num <= 55);
 
-for (let num = 50; num <= 55; num++) {
-	console.log(num); 
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Последний фильм, который вы посмотрели?', ''),
+		b = prompt('Ваша оценка:', '');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+
+
 }
+
+if (personalMovieDB.count < 10) {
+	console.log('Мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классически зритель');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Мало фильмов');
+} else {
+	console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
+
