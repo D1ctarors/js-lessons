e24f3d4da4b3e8c1ad1b21c4a26ecd222480892a
Dima <d1ctators74@yandex.ru>
2022-05-09 18:28:14 +0300

    pract-1

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,39 +1,22 @@
 "use strict";
-// // ===============================
-// const calc = [];
-// calc[0] = +prompt('Введите число "a"');
-// calc[1] = +prompt('Введите число "b"');
-// calc[2] = +prompt('Введите число "c"');
 
-// const sum = calc[0] + calc[1] + calc[2];
-// const umj = calc[0] * calc[1] * calc[2];
-// const min = calc[0] - calc[1] - calc[2];
-// const del = calc[0] / calc[1] / calc[2];
+const numberOfFilms = +prompt('Сволько фильмов уже посмотрели?', '');
 
-// console.log('Результат суммы: ' + sum);
-// console.log('Результат умножения: ' + umj);
-// console.log('Результат вычитания: ' + min);W
-// console.log('Результат деления: ' + del);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-// // ===============================
-// let incr = 10,
-// 	decr = 10;
+const a = prompt('Последний фильм, который вы посмотрели?', ''),
+		b = prompt('Ваша оценка: ', ''),
+		c = prompt('Последний фильм, который вы посмотрели?', ''),
+		d = prompt('Ваша оценка: ', '');
 
-// console.log(++incr);
-// console.log(--decr);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// // ===============================
-// console.log(5 % 2);
-
-// // ===============================
-// console.log(2 * 4 === '8');
-
-// // ===============================
-// const isChecked = true,
-// 	isClose = true;
-
-// console.log(isChecked && isClose);
-// console.log(isChecked || isClose);
-
-// // ===============================
+console.log(personalMovieDB);
 
