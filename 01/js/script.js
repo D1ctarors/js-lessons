73a51f18f9b4409e19f823c5903d112c2a2c7725
Dima <d1ctators74@yandex.ru>
2022-05-09 19:13:29 +0300

    les11

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,32 +1,18 @@
 "use strict";
 
-if (1) {
-	console.log('OK');
-} else {
-	console.log('Error');
-}
+// let num = 50;
 
-const num = 50;
-
-// if(num < 49) {
-// 	console.log('error');
-// }	else if (num > 100) {
-// 	console.log('Много');
-// } else {
-// 	console.log('ok');
+// while (num <= 55) {
+// 	console.log(num);
+// 	num++;
 // }
 
-// (num === 50) ? console.log('ok') : console.log('error');
+// do {
+// 	console.log(num);
+// 	num++;
+// }
+// while (num <= 55);
 
-
-switch (num) {
-	case 49:
-		console.log('err');
-		break;
-	case 100:
-		console.log('err');
-		break;
-	case 50:
-		console.log('ok');
-		break;
+for (let num = 50; num <= 55; num++) {
+	console.log(num); 
 }
